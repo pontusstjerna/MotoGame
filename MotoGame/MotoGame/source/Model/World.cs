c26e095491fa2d03dfc9752e4d23e71d00cacced
Pontus Stjernström <pontus@stjernstrom.nu>
2017-05-18 09:29:57 +0200

    Added boiler plate code and also some simple classes for rendering a sequence of lines.

---
+++ MotoGame/MotoGame/source/Model/World.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MotoGame.Model
+{
+    public class World
+    {
+        private List<Point> points;
+        public List<Point> Points {
+            get {
+                return points;
+            }
+        }
+
+        public World()
+        {
+            points = new List<Point>();
+            points.Add(new Point(10, 50));
+            points.Add(new Point(50, 50));
+            points.Add(new Point(100, 200));
+            points.Add(new Point(500, 300));
+            points.Add(new Point(600, 300));
+        }
+        
+    }
+}
---
+++ MotoGame/MotoGame/source/View/WorldRenderer.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using MotoGame.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MotoGame.View
+{
+    public class WorldRenderer
+    {
+        private World world;
+        private Texture2D lineTexture;
+
+        public WorldRenderer(World world, GraphicsDevice gd)
+        {
+            this.world = world;
+            lineTexture = new Texture2D(gd, 1, 1);
+            lineTexture.SetData<Color>(new Color[] { Color.Black });
+        }
+
+        public void RenderWorld(SpriteBatch sb)
+        {
+            sb.Begin();
+            List<Point> points = world.Points;
+            for(int i = 0; i < points.Count() - 1; i++)
+            {
+                DrawLine(sb, points[i], points[i+1]);
+            }
+            sb.End();
+        }
+
+       private void DrawLine(SpriteBatch sb, Point start, Point end)
+        {
+            Vector2 edge = end.ToVector2() - start.ToVector2();
+            float angle = (float)Math.Atan2(edge.Y, edge.X);
+
+            sb.Draw(lineTexture,
+                new Rectangle(start.X, start.Y, (int)edge.Length(), 1),
+                null, Color.Black, angle, new Vector2(0, 0), SpriteEffects.None, 0);
+        }
+    }
+}
