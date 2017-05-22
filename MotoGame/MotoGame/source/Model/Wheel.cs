60075909975a86605efe3c6f39875ff180e012d0
Pontus Stjernström <pontus@stjernstrom.nu>
2017-05-22 07:20:07 +0200

    Fixed collision to ground better. Moved logic to wheel class.

--- MotoGame/MotoGame/source/Model/Wheel.cs
+++ MotoGame/MotoGame/source/Model/Wheel.cs
@@ -16,8 +16,17 @@ namespace MotoGame.source.Model
             }
         }
 
-        public float radius = 4;
-        public float friction { get; private set; } = 0.1f;
+        public Vector2 Velocity
+        {
+            get
+            {
+                return velocity;
+            }
+        }
+
+        public float Radius = 8;
+        public float Bounciness { get; private set; } = 0.1f;
+        public bool OnGround { get; set; } = false;
 
         private Vector2 position;
         private Vector2 velocity;
@@ -35,19 +44,64 @@ namespace MotoGame.source.Model
             position.X += velocity.X * dTime;
             position.Y += velocity.Y * dTime;
 
-            ApplyGravity();
+            if(!OnGround)
+                ApplyGravity();
         }
 
-        public void SetVX(float vx)
+        public void SetOnGround(Vector2 groundBelow)
         {
-            velocity.X = vx;
+            velocity.Y = 0;
+            position.Y = groundBelow.Y - Radius;
+            OnGround = true;
         }
 
-        public void SetVY(float vy)
+        public void Bounce(Point a, Point b, Vector2 groundBelow)
         {
-            velocity.Y = vy;
+            Vector2 lineSegment = new Vector2(b.X - a.X, b.Y - a.Y);
+
+            //To get this, take the orthogonal vector of the line segment
+            Vector2 collisionVector = new Vector2(-lineSegment.Y, lineSegment.X);
+
+            Vector2 wheelDir;
+
+            /*
+             * var collisionVector = new Vector2D(ball2.x - ball1.x, ball2.y - ball1.y);
+                    var orthoCollisionVector = new Vector2D(-collisionVector.y, collisionVector.x);
+
+                    var colDir = collisionVector.unitVector();
+                    var unaffectDir = orthoCollisionVector.unitVector();
+
+                    var ball1Dir = new Vector2D(ball1.vx, ball1.vy);
+                    var ball2Dir = new Vector2D(ball2.vx, ball2.vy);
+
+                    var u1 = dot(ball1Dir,colDir);
+                    var u2 = dot(ball2Dir,colDir);
+
+                    var unaffected1 = unaffectDir.getScalar(dot(ball1Dir, unaffectDir));
+                    var unaffected2 = unaffectDir.getScalar(dot(ball2Dir, unaffectDir));
+
+                    audio.pause();
+                    audio.currentTime = 0;
+                    audio.play();
+                    
+                    var m1 = ball1.mass;
+                    var m2 = ball2.mass;
+                    var I = m1*u1 + m2*u2;
+                    var R = -(u2-u1);
+                    
+
+            var v2 = (I + m1 * R) / (m1 + m2);
+            var v1 = v2 - R;
+
+            ball1.vx = (colDir.x * v1 + unaffected1.x) * (1 - friction);
+            ball1.vy = (colDir.y * v1 + unaffected1.y) * (1 - friction);
+            ball2.vx = (colDir.x * v2 + unaffected2.x) * (1 - friction);
+            ball2.vy = (colDir.y * v2 + unaffected2.y) * (1 - friction);
+
+            p("ballsTotVel before: " + (ball1Dir.length + ball2Dir.length) + " After: " + (new Vector2D(ball1.vx, ball1.vy).length + new Vector2D(ball2.vx, ball2.vy).length));
+            * */
         }
-        
+
         private void ApplyGravity()
         {
             velocity.Y += 9.81f;
--- MotoGame/MotoGame/source/Model/World.cs
+++ MotoGame/MotoGame/source/Model/World.cs
@@ -43,74 +43,27 @@ namespace MotoGame.Model
         {
             for(int i = 0; i < points.Count() - 1; i++)
             {
-                if(GetIntersection(Bike.rearWheel, points[i], points[i + 1]))
+                Vector2? intersection = GetIntersection(Bike.rearWheel, points[i], points[i + 1]);
+                if (intersection != null)
                 {
-                    //On ground
-                    Bike.rearWheel.SetVY(0);
+                    Bike.rearWheel.SetOnGround(intersection.GetValueOrDefault());
+                    Bike.rearWheel.Bounce(points[i], points[i+1], intersection.GetValueOrDefault())
                     return;
                 }
             }
         }
-
-        private void Bounce(Wheel wheel, Point a, Point b)
+        
+        private Vector2? GetIntersection(Wheel wheel, Point a, Point b)
         {
-            Vector2 lineSegment = new Vector2(b.X - a.X, b.Y - a.Y);
-            
-            //To get this, take the orthogonal vector of the line segment
-            Vector2 collisionVector = new Vector2(-lineSegment.Y, lineSegment.X);
 
-            Vector2 wheelDir;
-
-            /*
-             * TODO:: PUT THIS METHOD IN THE WHEEL CLASS INSTEAD
-            */
-
-            /*
-             * var collisionVector = new Vector2D(ball2.x - ball1.x, ball2.y - ball1.y);
-                    var orthoCollisionVector = new Vector2D(-collisionVector.y, collisionVector.x);
-
-                    var colDir = collisionVector.unitVector();
-                    var unaffectDir = orthoCollisionVector.unitVector();
-
-                    var ball1Dir = new Vector2D(ball1.vx, ball1.vy);
-                    var ball2Dir = new Vector2D(ball2.vx, ball2.vy);
-
-                    var u1 = dot(ball1Dir,colDir);
-                    var u2 = dot(ball2Dir,colDir);
-
-                    var unaffected1 = unaffectDir.getScalar(dot(ball1Dir, unaffectDir));
-                    var unaffected2 = unaffectDir.getScalar(dot(ball2Dir, unaffectDir));
-
-                    audio.pause();
-                    audio.currentTime = 0;
-                    audio.play();
-                    
-                    var m1 = ball1.mass;
-                    var m2 = ball2.mass;
-                    var I = m1*u1 + m2*u2;
-                    var R = -(u2-u1);
-                    
-
-            var v2 = (I + m1 * R) / (m1 + m2);
-            var v1 = v2 - R;
-
-            ball1.vx = (colDir.x * v1 + unaffected1.x) * (1 - friction);
-            ball1.vy = (colDir.y * v1 + unaffected1.y) * (1 - friction);
-            ball2.vx = (colDir.x * v2 + unaffected2.x) * (1 - friction);
-            ball2.vy = (colDir.y * v2 + unaffected2.y) * (1 - friction);
-
-            p("ballsTotVel before: " + (ball1Dir.length + ball2Dir.length) + " After: " + (new Vector2D(ball1.vx, ball1.vy).length + new Vector2D(ball2.vx, ball2.vy).length));
-            * */
-        }
-
-        private bool GetIntersection(Wheel wheel, Point a, Point b)
-        {
+            Vector2 wheelDir = wheel.Velocity;
+            wheelDir.Normalize();
 
             //A vector is wheel, b is slope
             float p0_x = wheel.Position.X;
             float p0_y = wheel.Position.Y;
-            float p1_x = wheel.Position.X + wheel.radius;
-            float p1_y = wheel.Position.Y + wheel.radius;
+            float p1_x = wheel.Position.X + wheelDir.X * wheel.Radius;
+            float p1_y = wheel.Position.Y + wheelDir.Y * wheel.Radius;
             float p2_x = a.X;
             float p2_y = a.Y;
             float p3_x = b.X;
@@ -123,7 +76,12 @@ namespace MotoGame.Model
 
             float s = (-s1_y * (p0_x - p2_x) + s1_x * (p0_y - p2_y)) / (-s2_x * s1_y + s1_x * s2_y);
             float t = (s2_x * (p0_y - p2_y) - s2_y * (p0_x - p2_x)) / (-s2_x * s1_y + s1_x * s2_y);
-            return (s >= 0 && s <= 1 && t >= 0 && t <= 1);
+
+            if (s >= 0 && s <= 1 && t >= 0 && t <= 1)
+            {
+                return new Vector2(p0_x + (t * s1_x), p0_y + (t * s1_y));
+            }
+            else return null;
         }
         
     }
