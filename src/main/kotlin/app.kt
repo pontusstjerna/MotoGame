009f32eb3d3001b68713c28b4c6595016fdee2a1
pontusstjerna <pontusstjernstrom@gmail.com>
2019-11-05 07:01:17 +0100

    add maven and update gitignore

---
+++ src/main/kotlin/app.kt
@@ -0,0 +1,12 @@
+/*
+* Some tutorials I used:
+* https://github.com/Quillraven/SimpleKtxGame/wiki/Application
+* https://github.com/libgdx/libgdx/wiki/A-simple-game
+* */
+
+    fun main(args: Array<String>) {
+        //Example().run()
+
+
+
+    }
