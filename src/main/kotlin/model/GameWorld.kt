2e37990e12dcc250a29dc50b2ca7c179979f58bd
Pontus Stjernström <pontus@stjernstrom.nu>
2019-11-12 06:47:15 +0100

    ADD SMOOOOOTH TERRAINgit add .git add .!

--- src/main/kotlin/model/GameWorld.kt
+++ src/main/kotlin/model/GameWorld.kt
@@ -11,28 +11,21 @@ import kotlin.random.Random
 class GameWorld {
 
     val timeStep: Float = 1.0f / 60.0f
-    val generationThreshold = 5;
 
     val physicsWorld: World = createWorld(gravity = Vector2(0f, -9.81f))
 
     val segments: MutableList<Segment> = mutableListOf(
             Segment(from = Vector2(1.0f, 5f), to = Vector2(10f, 4f), world = physicsWorld)
-    ).let {
-        for(i in 1..10) {
-            val last = it.last()
-            it.add(Segment(
-                    from = last.to,
-                    to = Vector2(last.to.x + 2f, last.to.y + Random.nextInt(-2, 3)),
-                    world = physicsWorld
-            ))
-        }
-        it
-    }
+    )
 
     val bike: Bike = Bike(Vector2(10f, 7f), physicsWorld)
 
+    private var dy: Float = 0f
+    private var ddy: Float = 0f
+
     private val VELOCITY_ITERATIONS = 8
     private val POSITION_ITERATIONS = 3
+    private val generationThreshold = 5;
 
     fun update() {
         physicsWorld.step(timeStep, VELOCITY_ITERATIONS, POSITION_ITERATIONS)
@@ -44,9 +37,12 @@ class GameWorld {
             val last = segments.last()
             segments.add(Segment(
                     from = last.to,
-                    to = Vector2(last.to.x + 2f, last.to.y + Random.nextInt(-2, 3)),
+                    to = Vector2(last.to.x + .5f, last.to.y + dy),
                     world = physicsWorld
             ))
+            dy += ((Random.nextFloat() - 0.5f) * .1f )
+            //dy += ddy * 0.1f
+            //ddy = ddy + Random.nextFloat() - 0.5f
         }
     }
 
