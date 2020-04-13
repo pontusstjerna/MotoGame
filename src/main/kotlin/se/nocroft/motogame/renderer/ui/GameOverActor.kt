153db53373b4f1698c228d899ff319114bc10efc
Pontus Stjernström <pontus@stjernstrom.nu>
2020-04-13 13:44:36 +0200

    make GameOverActor use BaseMenuActor

--- src/main/kotlin/se/nocroft/motogame/renderer/ui/GameOverActor.kt
+++ src/main/kotlin/se/nocroft/motogame/renderer/ui/GameOverActor.kt
@@ -4,6 +4,7 @@ import com.badlogic.gdx.graphics.g2d.BitmapFont
 import com.badlogic.gdx.scenes.scene2d.ui.Label
 import com.badlogic.gdx.scenes.scene2d.ui.Table
 import com.badlogic.gdx.scenes.scene2d.ui.Value
+import com.badlogic.gdx.utils.compression.lzma.Base
 import ktx.actors.onClick
 import se.nocroft.motogame.DEBUG
 import se.nocroft.motogame.PADDING_MEDIUM
@@ -11,44 +12,57 @@ import se.nocroft.motogame.TEXT_BUTTON_COLOR
 import se.nocroft.motogame.screen.GameService
 import kotlin.random.Random
 
-class GameOverActor(private val gameService: GameService, labelStyle: Label.LabelStyle): Table() {
-
-    private val gameOverTexts: Array<String> = arrayOf(
-            "Whoops, crashy!",
-            "Hey, you died now.",
-            "Uh-oh!",
-            "That was not supposed to happen...",
-            "Well played, well played...",
-            "GAME OVER MOHAHA",
-            "Whoops, you deaded!",
-            "Ouch, that must've hurt.",
-            "Better luck next time!",
-            "Don't try that at home.",
-            "Oh crap, who designed this game??"
-    )
+class GameOverActor(private val gameService: GameService, labelStyle: Label.LabelStyle): BaseMenuActor() {
 
     private val buttonLabelStyle = Label.LabelStyle().apply {
         font = BitmapFont()
         fontColor = TEXT_BUTTON_COLOR
     }
 
+    override val buttons = arrayOf(
+            Button("Retry", buttonLabelStyle).apply {
+                onPress {
+                    gameService.reset()
+                }
+                selected = true
+            },
+            Button("Exit", buttonLabelStyle).apply {
+                onPress {
+                    gameService.exitToMenu()
+                }
+            }
+    )
+
+    private val gameOverTexts: Array<String> = arrayOf(
+            "Whoops, crashy!",
+            "Hey, you died now.",
+            "Uh-oh!",
+            "That was not supposed to happen...",
+            "GAME OVER MOHAHA",
+            "Whoops, you deaded!",
+            "Ouch, that must've hurt.",
+            "Better luck next time!",
+            "Don't try that at home.",
+            "Oh crap, who designed this game??",
+            "Don't hug the ground.",
+            "Maybe noclip is something for you?",
+            "How about not diving into the ground.",
+            "Sure hope you were wearing a helmet.",
+            "So this is what it feels like to die.",
+            "Something went terribly wrong."
+    )
+
     private val gameOverLabel = Label("Whoops, you deaded. ", labelStyle)
     private val scoreLabel = Label("Your score: 0m", labelStyle)
-    private val retryButton = Button("Retry", buttonLabelStyle).apply {
-        onClick {
-            gameService.reset()
-        }
-    }
-
-    private val exitButton = Button("Exit", buttonLabelStyle)
 
     init {
         add(gameOverLabel)
         row()
         add(scoreLabel).pad(0f, 0f, PADDING_MEDIUM, 0f)
         row()
-        add(retryButton).padBottom(Value.percentHeight(12.5f))
-        //add(exitButton)
+        add(buttons[0])
+        row()
+        add(buttons[1]).padBottom(Value.percentHeight(12.5f))
         setFillParent(true)
     }
 
